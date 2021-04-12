bd0e9702d8a26d9b6469f30104365c4c5a283958
Viot Jules <jules.viot@etu.umontpellier.fr>
2021-04-12 21:36:39 +0200

    affichage du fragment home dans le fragment_container de MainActivity

--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/MainActivity.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import iut.montpellier.appdietetique.fragments.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // remplacement du fragment_container par le fragment home
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).addToBackStack(null).commit();
     }
+
 }
