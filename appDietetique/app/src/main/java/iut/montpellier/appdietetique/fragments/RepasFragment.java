83fbbbddf2d6c0f4683642f97290f6bd6fcac850
Viot Jules <jules.viot@etu.umontpellier.fr>
2021-04-21 16:30:57 +0200

    associer a l'image back arrow l'action de retourner a la journee

--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/RepasFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/RepasFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
 
 import iut.montpellier.appdietetique.R;
 
@@ -15,6 +19,16 @@ public class RepasFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_repas,container, false);
+        View view = inflater.inflate(R.layout.fragment_repas,container, false);
+
+        // associer a l'image back arrow l'action de retourner a la journee
+        ImageView backButton = view.findViewById(R.id.back_icon);
+        backButton.setClickable(true);
+        backButton.setOnClickListener(view1 -> {
+            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new HomeFragment());
+            fragmentTransaction.commit();
+        });
+        return view;
     }
 }
