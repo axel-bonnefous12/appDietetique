e054ebc414c615449bcfafc7db3986f15fd288cd
Viot Jules <jules.viot@etu.umontpellier.fr>
2021-04-22 16:40:41 +0200

    factorisation et addaptation au parcelable de la class HomeFragment et RepasFragment

--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/HomeFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package iut.montpellier.appdietetique.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.util.Objects;
+import java.util.Calendar;
 
 import iut.montpellier.appdietetique.R;
+import iut.montpellier.appdietetique.models.Journee;
+import iut.montpellier.appdietetique.models.Repas;
 
 public class HomeFragment extends Fragment {
 
@@ -22,41 +26,49 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container, false);
 
-        //passage au fragment repas lorsqu'on clique sur le bouton petit dej
-        Button boutonPetitDej = view.findViewById(R.id.button_petit_dej); // associer variable du bouton avec bouton graphique
-        boutonPetitDej.setOnClickListener(v -> {
-            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new RepasFragment());
-            fragmentTransaction.commit();
-        });
+        Journee journee = new Journee(Calendar.getInstance().getTime());
 
-        //passage au fragment repas lorsqu'on clique sur le bouton collation
-        Button boutonCollation = view.findViewById(R.id.button_collation); // associer variable du bouton avec bouton graphique
-        boutonCollation.setOnClickListener(v -> {
-            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new RepasFragment());
-            fragmentTransaction.commit();
-        });
+        //initialisation du bouton petit dej
+        Button boutonPetitDej = initBoutonsRepas(view, R.id.button_petit_dej, journee.getPetitDejeuner());
 
-        //passage au fragment repas lorsqu'on clique sur le bouton dejeuner
-        Button boutonDejeuner = view.findViewById(R.id.button_dejeuner); // associer variable du bouton avec bouton graphique
-        boutonDejeuner.setOnClickListener(v -> {
-            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new RepasFragment());
-            fragmentTransaction.commit();
-        });
+        //initialisation du bouton collation
+        Button boutonCollation = initBoutonsRepas(view, R.id.button_collation, journee.getCollation());
 
-        //passage au fragment repas lorsqu'on clique sur le bouton diner
-        Button boutonDiner = view.findViewById(R.id.button_diner); // associer variable du bouton avec bouton graphique
-        boutonDiner.setOnClickListener(v -> {
-            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new RepasFragment());
-            fragmentTransaction.commit();
-        });
+        //initialisation du bouton dejeuner
+        Button boutonDejeuner = initBoutonsRepas(view, R.id.button_dejeuner, journee.getDejeuner());
+
+        //initialisation du bouton diner
+        Button boutonDiner = initBoutonsRepas(view, R.id.button_diner, journee.getDiner());
 
         return view;
     }
 
+
+    /**
+     * @param view view de la page fragment_home
+     * @param idBouton id du bouton au quelle on veut associer l'action
+     * @param repas repas que l'ont veut afficher sur la page repas (PetitDejeuner, Collation, Dejeuner ou Diner)
+     * @return un bouton qui change le fragment actuel par le fragment repas, avec les données du repas passé en parametre
+     */
+    private Button initBoutonsRepas(View view, int idBouton, Repas repas){
+        //Récupération du bouton sur la view
+        Button bouton = view.findViewById(idBouton);
+
+        // Associer d'une action lorque le bouton est cliqué
+        bouton.setOnClickListener(v -> {
+            // Création du bundle d'un bundle de données avec l'objet que l'on veut passé en argument du fragment repas
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("data", repas);
+
+            // Initialisation de la transaction de fragment
+            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+            RepasFragment repasFragment = new RepasFragment();// creation de l'entite repasFragement
+            repasFragment.setArguments(bundle); //associer le bundle de données
+            fragmentTransaction.replace(R.id.fragment_container, repasFragment); // remplacer le fragment actuel avec le fragment repas
+            fragmentTransaction.commit(); //commit du fragment
+        });
+        return bouton;
+    }
 
 
 
--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/RepasFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/RepasFragment.java
@@ -1,10 +1,15 @@
 package iut.montpellier.appdietetique.fragments;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,12 +19,21 @@ import androidx.fragment.app.FragmentTransaction;
 import java.util.Objects;
 
 import iut.montpellier.appdietetique.R;
+import iut.montpellier.appdietetique.models.Repas;
 
 public class RepasFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_repas,container, false);
+
+        // Récupération des data passé en argument
+        Bundle bundle = this.getArguments();
+        Repas data = bundle.getParcelable("data");
+
+        // Mise à jour du champ de text titre
+        TextView titre = view.findViewById(R.id.titrePage);
+        titre.setText(data.getTypeDuRepas());
 
         // associer a l'image back arrow l'action de retourner a la journee
         ImageView backButton = view.findViewById(R.id.back_icon);
