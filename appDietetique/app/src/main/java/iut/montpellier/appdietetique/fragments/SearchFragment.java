b5e48685de59a0835e43a35c7fcebd1505c44b58
axel-bonnefous12 <axel.bonnefous01@etu.umontpellier.fr>
2021-05-05 23:39:21 +0200

    mise en forme page recherche

--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/SearchFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/SearchFragment.java
@@ -2,11 +2,15 @@ package iut.montpellier.appdietetique.fragments;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.security.PKCS12Attribute;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -25,6 +30,7 @@ import iut.montpellier.appdietetique.BDD.MySQLite;
 import iut.montpellier.appdietetique.BDD.PlatManager;
 import iut.montpellier.appdietetique.R;
 
+import iut.montpellier.appdietetique.adapters.PlatAfficherAdapter;
 import iut.montpellier.appdietetique.models.Journee;
 import iut.montpellier.appdietetique.models.Plat;
 import iut.montpellier.appdietetique.models.Repas;
@@ -33,10 +39,10 @@ import static iut.montpellier.appdietetique.BDD.DbUserManager.KEY_DATE;
 
 public class SearchFragment extends Fragment
 {
-    MySQLite affichageDb;
+    PlatManager affichageDb; // Déclaré comme variable globale
 
     ListView listView_affichageBDD;
-
+    EditText editText_search_id;
 
     @Nullable
     @Override
@@ -44,19 +50,54 @@ public class SearchFragment extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_search,container, false);
 
-        affichageDb = new MySQLite(this.getContext()); // Ouvre la bdd des plats
+        affichageDb = new PlatManager(this.getContext()); // Instance la bdd des plats
+        listView_affichageBDD = view.findViewById(R.id.affichageBDD);
 
-        initListViews(view, affichageDb);
+        initEditTexte(view);
 
         return view;
     }
 
-    private void initListViews(View view, MySQLite affichageDb)
-    {
-        listView_affichageBDD = view.findViewById(R.id.liste_de_plat);
-        ArrayList<String> listPlat = affichageDb.findPlat("Plats");
-        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this.getContext(), );
-        //listView_affichageBDD.setAdapter(listPlat);
 
+    private void initEditTexte(View view)
+    {
+        editText_search_id = view.findViewById(R.id.edit_text_search_id);
+        editText_search_id.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                affichageDb.open();
+                actualiseListView(view, affichageDb.getPlatNom("" + editText_search_id.getText()));
+                affichageDb.close();
+            }
+        });
     }
+
+    private void actualiseListView(View view, ArrayList<Plat> plats) // Actualiser la View
+    {
+        listView_affichageBDD.setAdapter(new PlatAfficherAdapter(getContext(), plats));
+        listView_affichageBDD.invalidateViews();
+    }
+
+
+
+
+
+
+
+
+
+
 }
