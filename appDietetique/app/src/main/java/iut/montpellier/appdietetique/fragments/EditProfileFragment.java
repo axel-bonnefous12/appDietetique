8ecd6faaf3ce530c2bf69de210e363d338e78f89
luc-lecoutour <77966986+luc-lecoutour@users.noreply.github.com>
2021-05-13 00:58:07 +0200

    profile user data

---
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/EditProfileFragment.java
@@ -0,0 +1,94 @@
+package iut.montpellier.appdietetique.fragments;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Objects;
+
+import iut.montpellier.appdietetique.MainActivity;
+import iut.montpellier.appdietetique.R;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
+public class EditProfileFragment extends Fragment {
+    View view;
+    private EditText inputName;
+    private EditText inputSurname;
+    private EditText inputAge;
+    private EditText inputPoids;
+    private EditText inputSexe;
+    private SharedPreferences prefs;
+    private Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view= inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        this.button = view.findViewById(R.id.btn_sauvegarder);
+
+        prefs = getContext().getSharedPreferences("MY_DATA",MODE_PRIVATE);
+        String nom = prefs.getString("MON_NOM","");
+        String prenom = prefs.getString("MON_PRENOM","");
+        int age = prefs.getInt("MON_Age",0);
+        int poids = prefs.getInt("MON_POIDS", 0);
+        String sexe = prefs.getString("MON_SEXE","");
+
+
+        inputName =(EditText) view.findViewById(R.id.inputName);
+        inputSurname =(EditText) view.findViewById(R.id.inputSurname);
+        inputAge =(EditText) view.findViewById(R.id.inputAge);
+        inputPoids =(EditText) view.findViewById(R.id.inputPoids);
+        inputSexe =(EditText) view.findViewById(R.id.inputSexe);
+        //on set les sharedPref
+        inputName.setText(nom);
+        inputSurname.setText(prenom);
+        inputAge.setText(age+"");
+        inputPoids.setText(poids+"");
+        inputSexe.setText(sexe);
+
+        //bouton de validation et sauvegarde
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //on get les input Text
+                String nom=inputName.getText().toString();
+                String prenom=inputSurname.getText().toString();
+                int age=Integer.parseInt(inputAge.getText().toString());
+                int poids=Integer.parseInt(inputPoids.getText().toString());
+                String sexe=inputSexe.getText().toString();
+
+
+                //sauvegarde des datas
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("MON_NOM",nom);
+                editor.putString("MON_PRENOM",prenom);
+                editor.putInt("MON_AGE",age);
+                editor.putInt("MON_POIDS",poids);
+                editor.putString("MON_SEXE",sexe);
+                editor.apply();
+                //return to profile
+                FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+                UserFragment userFragment = new UserFragment();//
+
+                fragmentTransaction.replace(R.id.fragment_container, userFragment); // remplacer le fragment actuel avec le fragment repas
+                fragmentTransaction.commit(); //commit du fragment
+
+            }
+        });
+        return  view;
+    }
+}
--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/UserFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/UserFragment.java
@@ -2,6 +2,8 @@ package iut.montpellier.appdietetique.fragments;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
@@ -15,8 +17,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
 
 import iut.montpellier.appdietetique.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class UserFragment extends Fragment {
      View view;
@@ -25,7 +32,7 @@ public class UserFragment extends Fragment {
     private TextView age;
     private TextView poids;
     private TextView sexe;
-    private String m_text="";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -35,35 +42,31 @@ public class UserFragment extends Fragment {
         age = view.findViewById(R.id.champAge);
         poids = view.findViewById(R.id.champPoids);
         sexe = view.findViewById(R.id.champSexe);
-        Button button =(Button) view.findViewById(R.id.btn_modif_profil);
-        
-        // changer un champs texte de l'user Pour l'instant j'ai trouvé que le prénom
+        Button button =(Button) view.findViewById(R.id.btn_profile);
+
+        SharedPreferences prefs = getContext().getSharedPreferences("MY_DATA",MODE_PRIVATE);
+        String name=prefs.getString("MON_NOM","pas de nom");
+        String surname=prefs.getString("MON_PRENOM","pas de Prenom");
+        int monAge=prefs.getInt("MON_AGE", 0);
+        int monPoids=prefs.getInt("MON_POIDS", 0);
+        String monSexe=prefs.getString("MON_SEXE","votre sexe");
+
+        this.nom.setText(name);
+        this.prenom.setText(surname);
+        this.age.setText(monAge+"");
+        this.poids.setText(monPoids+"");
+        this.sexe.setText(monSexe);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                final EditText inputNom= new EditText(view.getContext());
-                inputNom.setInputType(InputType.TYPE_CLASS_TEXT);
-                builder.setView(inputNom);
-                builder.setView(inputNom);
-                builder.setMessage("changer le nom?");
-                builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        m_text= inputNom.getText().toString();
-                        nom.setText(m_text);
-                        Toast.makeText(view.getContext(), "changement fait", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                builder.show();
+                FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+                EditProfileFragment editProfileFragment = new EditProfileFragment();
+
+                fragmentTransaction.replace(R.id.fragment_container, editProfileFragment); // remplacer le fragment actuel avec le fragment repas
+                fragmentTransaction.commit(); //commit du fragment
             }
         });
         return view;
     }
-    /*public void changerInfo(View v){
-        final EditText inputNom= new EditText(getContext());
-        inputNom.setInputType(InputType.TYPE_CLASS_TEXT);
-        //nom.setText(inputNom.getText());
-        //AlertDialog.Builder popUp = new AlertDialog.Builder(new AlertDialog().Builder(view));
-    }*/
+
 }
