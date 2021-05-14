7d6cd88cba19438b04b778bcc873ea64e4736a83
axel-bonnefous12 <axel.bonnefous01@etu.umontpellier.fr>
2021-05-14 11:36:59 +0200

    AfficherPlatFragment done

--- appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/AfficherPlatFragment.java
+++ appDietetique/app/src/main/java/iut/montpellier/appdietetique/fragments/AfficherPlatFragment.java
@@ -3,66 +3,104 @@ package iut.montpellier.appdietetique.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Date;
+import java.util.Objects;
+
+import iut.montpellier.appdietetique.BDD.DbUserManager;
+import iut.montpellier.appdietetique.BDD.PlatManager;
 import iut.montpellier.appdietetique.R;
+import iut.montpellier.appdietetique.models.Plat;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AfficherPlatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AfficherPlatFragment extends Fragment
 {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View view;
+    int idPlat;
+    DbUserManager dbUserManager;
+    PlatManager platManager;
+    Plat plat;
 
-    public AfficherPlatFragment()
-    {
-        // Required empty public constructor
-    }
+    TextView textView_afficher_nomPlat;
+    TextView textView_totalProteines;
+    TextView textView_totalGlucides;
+    TextView textView_totalCalories;
+    ImageView button_back;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AfficherPlatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AfficherPlatFragment newInstance(String param1, String param2) {
-        AfficherPlatFragment fragment = new AfficherPlatFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_afficher_plat, container, false);
+        view = inflater.inflate(R.layout.fragment_afficher_plat, container, false);
+
+        Bundle bundle = this.getArguments(); // Récupération du bundle passé en argument
+
+        idPlat = bundle.getInt("idPlat");
+
+        dbUserManager = new DbUserManager(getContext());
+        platManager = new PlatManager(getContext());
+
+        trouverPlatDansBdd();
+        initText();
+        initBackButton();
+
+        return view;
     }
+
+    private void initText()
+    {
+        textView_afficher_nomPlat = view.findViewById(R.id.afficher_nomDuPlat);
+        textView_afficher_nomPlat.setText(plat.getNom());
+
+        // Mise à jour du champ de valeur de proteines
+        textView_totalProteines = view.findViewById(R.id.afficher_proteine);
+        textView_totalProteines.setText("" + plat.getProteines());
+
+        // Mise à jour du champ de valeur de glucides
+        textView_totalGlucides = view.findViewById(R.id.afficher_glucides);
+        textView_totalGlucides.setText("" + plat.getGlucides());
+
+        // Mise à jour du champ de valeur de calories
+        textView_totalCalories = view.findViewById(R.id.afficher_calories);
+        textView_totalCalories.setText("" + plat.getCalories());
+    }
+
+    private void trouverPlatDansBdd()
+    {
+        platManager.open();
+        plat = platManager.getPlatId(idPlat);
+        platManager.close();
+    }
+
+    private void initBackButton()
+    {
+        button_back = view.findViewById(R.id.back_icon);
+        button_back.setClickable(true);
+        button_back.setOnClickListener(view1 -> {
+            allerSearchFragment();
+        });
+    }
+
+    private void allerSearchFragment(){
+        SearchFragment searchFragment = new SearchFragment();
+
+        FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, searchFragment);
+        fragmentTransaction.commit();
+    }
+
+
+
 }
